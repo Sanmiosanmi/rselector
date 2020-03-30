9cbbb06c210892ba2e02f827748bd0a7f594528e
olasunkanmi ilesanmi <sunkyx99@gmail.com>
2020-03-30 23:43:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+//import React from 'react';
+import ReactDOM from 'react-dom';
+//import RandomSelector from './Components/RandomSelector';
+//import 'normalize.css/normalize.css'
+import './styles/style.scss';
+
+
+//import React from 'react';
+import AddOption from './Components/AddOption';
+import Action from './Components/Action';
+import Header from './Components/Header';
+import Options from './Components/Options';
+import OptionModal from './Components/OptionModal';
+
+class App extends React.Component {
+  state = {
+    options: [],
+    selectedOption: undefined
+  };
+  handleDeleteOptions = () => {
+    this.setState(() => ({ options: [] }));
+  };
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option)
+    }));
+  };
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    this.setState(() => ({
+      selectedOption: option
+
+
+    }));
+  };
+  clearSelectedOption = () => {
+    this.setState(() => ({
+      selectedOption: undefined
+
+
+
+    }))
+
+  };
+
+
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists';
+    }
+
+    this.setState((prevState) => ({
+      options: prevState.options.concat(option)
+    }));
+  };
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      // Do nothing at all
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+  render() {
+    const subtitle = 'This Randomly choose an option from the list';
+
+    return (
+      <div>
+
+        <Header subtitle={subtitle} />
+        <div className='container'>
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <div className='widgets'>
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
+        </div>
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          clearSelectedOption={this.clearSelectedOption}
+        />
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
---
+++ src/Components/Option.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Option = (props) => (
+  <div className='options'>
+    <p className='options__text'>{props.count}.  {props.optionText}</p>
+
+    <button
+      className='button button--link'
+      onClick={(e) => {
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+      Remove
+    </button> </div>
+
+
+);
+
+export default Option;
